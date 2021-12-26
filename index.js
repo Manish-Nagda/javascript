fe9d5e74656187b3a3dd2df3161393f5ff436e84
Manish-Nagda <92258245+Manish-Nagda@users.noreply.github.com>
2021-12-26 19:39:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,109 @@
+// var num1 = 30;
+// var num2 = 25;
+// console.log(num1-num2);
+// var str1= ("this is a string");
+// var str2=("this is also a string");
+// console.log(str2);
+// var marks={ravi:23,
+// shubham: 34, manish:65}
+// console.log(marks);
+// var a= true;
+// var b= false;
+// console.log (a,b);
+// var arr=[1,2,3,4,5]
+// console.log(arr[2]);
+// var a= 23;
+// var b= 34;
+// console.log("the value of a+b",a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// var c=a-b;
+// console.log(c);
+// function avg(a,b){
+//     return (a+b)/2;
+// }
+// // 
+// // let myarr=["fan","camera", "34","null","true"];
+// console.log(myarr);
+// myarr.pop()
+//  console.log(myarr);
+//  myarr.push("harry")
+//  console.log(myarr);
+//  myarr.shift()
+//  console.log(myarr);
+//  var sort= [1,2,13,24,32,7,8]
+//  console.log(sort);
+//  let String ="manish";
+//  console.log(String.slice(2,5));
+//  let = string .replace("manish","rohan");
+// //  console.log(let,string);
+//  let mydate = new Date();
+//  console.log(mydate);
+//  console.log(mydate.getTime());
+//  console.log(mydate.getFullYear());
+//  console.log(mydate.getDay());
+//  console.log(mydate.getMonth());
+//  console.log(mydate.getUTCDay());
+//  console.log(mydate.getSeconds());
+//  console.log(mydate.getUTCMinutes());
+//  console.log(mydate.toTimeString());
+//  console.log(mydate .getMonth());
+//  console.log (mydate.getDay());  
+//  console.log(mydate.getTime());
+//  var arr=[1,2,4,5,3,7,8,]
+//  console.log(arr);
+//  console.log(arr.reverse);
+//  const inventory = [
+//     {name: 'apples', quantity: 2},
+//     {name: 'bananas', quantity: 0},
+//     {name: 'cherries', quantity: 5}
+//   ];
+  
+//   function apples (fruit) {
+//     return fruit.name === 'apples';
+//   }
+  
+//   console.log(inventory.find(apples));
+//   function isPrime(element, index, array) {
+//     let start = 2;
+//     while (start <= Math.sqrt(element)) {
+//       if (element % start++ < 1) {
+//         return false;
+//       }
+//     }
+//     return element > 1;
+//   }
+  
+// //   console.log([4, 6, 8, 12].find(isPrime)); // undefined, not found
+// //   console.log([4, 5, 8, 12].find(isPrime)); // 
+// //   let array1 = ['one', 'two', 'three'];
+// //   console.log(array1);
+// //   array1.sort()
+//   console.log(array1);
+//   const array1=[1,2,7,9,6,0];
+//   array1.reverse();
+//   console.log(array1);
+//   
+// 
+// const months = ['Jan', 'March', 'April', 'June'];
+// months.splice(1, 0, 'Feb');
+// console.log(months);
+// let months=['jan','march','april','may'];
+// months.splice(3,1,'june');
+// console.log(months);
+// let animal=['cat','cow','dog', 'billi','kutta'];
+// console.log(animal.slice(4));
+// let array =[1,2,3,4,5];
+// let even=(element)=>element%2===0;
+// console.log(array.some(even));
+// const array1 = ['a', 'b', 'c'];
+// const iterator = array1.values();
+
+// for (const value of iterator) {
+//   console.log(value);
+// }
+const array1 = [1, 2, 3, 'a', '1a'];
+console.log(array1.toString());
+console.log(array1);
